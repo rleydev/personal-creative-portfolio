85f09bb770cd8cffc8237614dc292c4f06a92ebd
Arthur Lee <rley.dev@gmail.com>
2023-02-16 21:35:56 +0600

    feat: set sidebar nav links

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,47 @@
+import './index.scss';
+import { Link, NavLink } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser , faHome, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='nav-bar__logo' to='/'>
+            <img src={LogoS} alt='logo' />
+            <img className='nav-bar__logo--sub-logo' src={LogoSubtitle} alt='subtitle' />
+        </Link>
+        <nav>
+            <NavLink exact='true' activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact='true' activeclassname="active" className='about-link' to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact='true' activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target='_blank' rel='noreferrer' href='SOMELINK'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://github.com/rleydev'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href='somelink'>
+                    <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar
