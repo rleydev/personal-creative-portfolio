0816bf8b68f4f0540f995393fe60ee902975cc89
Arthur Lee <rley.dev@gmail.com>
2023-02-21 04:09:19 +0600

    feat: done about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,6 +3,7 @@ import AnimatedLetters from '../AnimatedLetters';
 import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
 
 const About = () => {
 
@@ -18,49 +19,53 @@ const About = () => {
         }, []);
 
     return(
-        <div className='container about-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters
-                    lettersClass={letterClass}
-                        strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
-                        idx={15} 
-                    />
-                </h1>
-                <p>
-                    I am very passionate about working on visual products 
-                    such as websites, videos, graphic images.
-                </p>
-                <p>
-                    I am confident quick learner, and constatnly working on my skills improvement
-                </p>
-                <p>
-                    Briefly about me, I am a creative tech-obsessed person!
-                </p>
-            </div>
-            <div className='stage-cube-cont'>
-                <div className='cubespinner'>
-                    <div className='face1'>
-                        <FontAwesomeIcon icon={faAngular} color='#DD0031' />
-                    </div>
-                    <div className='face2'>
-                        <FontAwesomeIcon icon={faHtml5} color='#F06529' />
-                    </div>
-                    <div className='face3'>
-                        <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
-                    </div>
-                    <div className='face4'>
-                        <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
-                    </div>
-                    <div className='face5'>
-                        <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
-                    </div>
-                    <div className='face6'>
-                        <FontAwesomeIcon icon={faGitAlt} color='#EC4D48' />
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                        lettersClass={letterClass}
+                            strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
+                            idx={15} 
+                        />
+                    </h1>
+                    <p>
+                        I am very passionate about working on visual products 
+                        such as websites, videos, graphic images.
+                    </p>
+                    <p>
+                        I am confident quick learner, and constatnly working on my skills improvement
+                    </p>
+                    <p>
+                        Briefly about me, I am a creative tech-obsessed person!
+                    </p>
+                </div>
+                <div className='stage-cube-cont'>
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faAngular} color='#DD0031' />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faHtml5} color='#F06529' />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faGitAlt} color='#EC4D48' />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <Loader type='pacman' />
+        </>
+        
     )
 }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters/index.js';
 import React, { useState, useEffect } from 'react';
 import Logo from './Logo';
+import Loader from 'react-loaders';
 
 const Home = () => {
 
@@ -21,25 +22,28 @@ const Home = () => {
     }, []);
 
     return (
-        <div className="container home-page">
-            <div className='text-zone'>
-                <h1>
-                    <span className={letterClass}>H</span>
-                    <span className={`${letterClass} _12`}>i,</span>
-                    <br />
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'m</span>
-                    <img src={LogoTitle} alt="developer" />
-                    <AnimatedLetters lettersClass={letterClass} strArray={nameArray} idx={15} />
-                    <br />
-                    <AnimatedLetters lettersClass={letterClass} strArray={jobArray} idx={22} />
-                    <br />
-                </h1>
-                <h2>Frontend Developer</h2>
-                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+        <>
+            <div className="container home-page">
+                <div className='text-zone'>
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                        <img src={LogoTitle} alt="developer" />
+                        <AnimatedLetters lettersClass={letterClass} strArray={nameArray} idx={15} />
+                        <br />
+                        <AnimatedLetters lettersClass={letterClass} strArray={jobArray} idx={22} />
+                        <br />
+                    </h1>
+                    <h2>Frontend Developer</h2>
+                    <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                </div>
+                <Logo />
             </div>
-            <Logo />
-        </div>
+            <Loader type='pacman' />
+        </>
     );
 }
 
