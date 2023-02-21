e1970636fe0661e5e9518f6e94639b67b2eafe02
Arthur Lee <rley.dev@gmail.com>
2023-02-21 22:54:04 +0600

    feat: add deploy

---
+++ src/components/About/index.js
@@ -0,0 +1,77 @@
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Loader from 'react-loaders';
+import { faReact, faAngular } from '@fortawesome/free-brands-svg-icons';
+import { faGuitar, faMusic, faVideo, faPaintBrush } from '@fortawesome/free-solid-svg-icons'
+
+const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        // Replace the class with a new one after a delay
+            const timeout = setTimeout(() => {
+                setLetterClass('text-animate-hover');
+            }, 3000);
+    
+            return () => clearTimeout(timeout);
+        }, []);
+
+    return(
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                        lettersClass={letterClass}
+                            strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
+                            idx={15} 
+                        />
+                    </h1>
+                    <p>
+                        I am passionate about visual products 
+                        such as websites, videos, audio, graphic images.
+                    </p>
+                    <p>
+                        I am confident quick learner, and constatnly working on my skills improvement. <br/><br />
+                        Developer skills: HTML, CSS, JavaScript, Typescript, SASS/SCSS, React, Angular, Swift, UIKit, Git
+                        Graphic software: Adobe Photoshop, Adobe Illustrator, Sketch, Figma, Adobe XD. <br />
+                        Audio software: Cockos Reaper, IZotope RX Audio Editor, Audio Plugins. <br />
+                        Video software: Final Cut Pro. 
+                    </p>
+                    <p>
+                        Briefly about me, I am a creative tech-obsessed person!
+                    </p>
+                </div>
+                <div className='stage-cube-cont'>
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faGuitar} color='#DD0031' />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faMusic} color='#F06529' />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faVideo} color='#28A4D9' />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faPaintBrush} color='#5ED4F4' />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faAngular} color='#EFD81D' />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faReact} color='#EC4D48' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Loader type='pacman' />
+        </>
+        
+    )
+}
+
+export default About
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,46 @@
+import './index.scss';
+import { Link, NavLink } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser , faHome, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='nav-bar__logo' to='/'>
+            <img src={LogoS} alt='logo' />
+            <p className='nav-bar__logo--sub-logo'>@rleydev</p>
+        </Link>
+        <nav>
+            <NavLink exact='true' activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact='true' activeclassname="active" className='about-link' to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact='true' activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.instagram.com/arleymi/'>
+                    <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://github.com/rleydev'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.youtube.com/channel/UCEwYA0MYOnJjch8lRZ8Z-3Q'>
+                    <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar
